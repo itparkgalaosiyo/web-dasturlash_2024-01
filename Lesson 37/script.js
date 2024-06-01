8f06ff0699c86cbc5fa82ba92515d9cc7dba2466
Doston Nabotov <dostonnabotov@gmail.com>
2024-06-01 20:08:29 +0500

    Add "Lesson 37"

---
+++ Lesson 37/script.js
@@ -0,0 +1,82 @@
+// variable
+let yosh = 19;
+let ism = "Bekzod";
+let bitiruvchi = true;
+
+// Agar bitiruvchi bo'lsa, YOKI yoshi 18 dan katta bo'lsa
+//    -> "Ishga qabul qilindingiz"
+
+// if (bitiruvchi == true) {
+//     console.log("Ishga qabul qilindingiz");
+// } else if (yosh >= 18) {
+//     console.log("Ishga qabul qilindingiz");
+// } else {
+//     console.log("Qabul qilinmadi!");
+// }
+
+// if (bitiruvchi == true || yosh >= 18) {
+//   console.log("Ishga qabul qilindingiz");
+// } else {
+//   console.log("Qabul qilinmadi!");
+// }
+
+// Agar ismi "Bekzod" bo'lsa YOKI bitiruvchi bo'lsa
+//     -> "Salom"
+// Bo'lmasa
+//     -> "Xayr!"
+
+// if (ism == "Bekzod" || bitiruvchi == true) {
+//   console.log("Salom");
+// } else {
+//   console.log("Xayr");
+// }
+
+// Ham bitiruvchi ham 18 dan katta bo'lsa
+//    -> "Ofarin!"
+// Bo'lmasa
+//    -> "Olg'a"
+
+// if (bitiruvchi == true && yosh >= 18) {
+//   console.log("Ofarin!");
+// } else {
+//   console.log("Olg'a");
+// }
+
+// if (yosh >= 18) {
+//     console.log("Ruxsat!");
+// } else {
+//     console.log("Ruxsat emas!");
+// }
+
+// faqat Bekzod = to'g'ri
+// hammasi (BEKzod, BEKZOD, bekzod) = noto'g'ri
+
+// if (ism == "Bekzod") {
+//     console.log("to'g'ri");
+// } else {
+//     console.log("noto'g'ri");
+// }
+
+// Agar bitiruvchi bo'lsa, "Tabriklayman!"
+// Bo'lmasa, "Olg'a!"
+
+// if (bitiruvchi == true) {
+//     console.log("Tabriklayman!");
+// } else {
+//     console.log("Olg'a!");
+// }
+
+// let qoldiq = 18 - yosh;
+// const id = 12345678;
+// const pi = 3.14;
+
+// console.log("Hello world!");
+// console.log(yosh);
+// console.log(yosh + 10);
+// console.log(id);
+
+// yosh = 18;
+
+// console.log(yosh - 25);
+// console.log(yosh + yosh);
+// console.log(qoldiq);
